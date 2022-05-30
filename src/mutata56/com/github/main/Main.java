bac276ce5a6c31e1070e5fbd3ba761ca4c2ec9f7
Khaliullin Cyrill <KKRLL56@yandex.ru>
2022-05-30 19:15:13 +0300

    Refactored the program. Fixed the program. Added hipsum text in Input.txt

--- src/mutata56/com/github/main/Main.java
+++ src/mutata56/com/github/main/Main.java
@@ -5,8 +5,8 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class Main {
-    private static final Path OUTPUT = Path.of("IO\\Output.txt");
-    private static final Path INPUT = Path.of("IO\\Input.txt");
+    private static final Path OUTPUT = Path.of("IO\\Output\\Output.txt");
+    private static final Path INPUT = Path.of("IO\\Input\\Input.txt");
     public static void main(String[] args) {
         switch (args[0]) {
             case "-c" -> someMethod();
@@ -16,26 +16,35 @@ public class Main {
     }
 
     private static void someMethod() {
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(OUTPUT.toFile(),true))) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(OUTPUT.toFile(),false))) {
+            int total = 0;
             for(String line : Files.readAllLines(INPUT)) {
-                writer.write(line.split("\\s").length);
-                writer.write("\n");
+                if(!line.isEmpty() && !line.isBlank()) {
+                    int length = line.split("\\s").length;
+                    total += length;
+                    writer.write(length + "");
+                    writer.write("\n");
+                }
             }
+            writer.write("Total words: " + total);
         } catch (IOException exception) {
             exception.printStackTrace();
         }
     }
     private static void findWords(String word) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(OUTPUT.toFile(),true))) {
+            int total = 0;
             for(String line : Files.readAllLines(INPUT)) {
                 int index = -1;
                 int count = 0;
                 while((index = line.indexOf(word,index)) >= 0) {
                     count++;
                 }
-                writer.write(count);
+                total+= count;
+                writer.write(count + "");
                 writer.write("\n");
             }
+            writer.write("Total words: " + total);
         } catch (IOException exception) {
             exception.printStackTrace();
         }
