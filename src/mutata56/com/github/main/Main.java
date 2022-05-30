f02ab31271e64a3de5e78094836d2cb02345509c
Khaliullin Cyrill <KKRLL56@yandex.ru>
2022-05-30 18:54:30 +0300

    Fixed bug in findWords. Refactored the code

--- src/mutata56/com/github/main/Main.java
+++ src/mutata56/com/github/main/Main.java
@@ -1,35 +1,36 @@
 package mutata56.com.github.main;
 
 import java.io.*;
+import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Arrays;
 
 public class Main {
-    private static Path PATH = Path.of("IO\\Output.txt");
+    private static final Path OUTPUT = Path.of("IO\\Output.txt");
+    private static final Path INPUT = Path.of("IO\\Input.txt");
     public static void main(String[] args) {
         switch (args[0]) {
-            case "-c" -> someMethod(Arrays.copyOfRange(args, 1, args.length));
-            case "-f" -> findWords(Arrays.copyOfRange(args, 2, args.length), args[1]);
+            case "-c" -> someMethod();
+            case "-f" -> findWords(args[1]);
             default -> throw new IllegalArgumentException("Couldn't resolve mode.");
         }
     }
 
-    private static void someMethod(String[] args) {
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(PATH.toFile(),true))) {
-            for(String str : args) {
-                writer.write(str.split("\\s").length);
+    private static void someMethod() {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(OUTPUT.toFile(),true))) {
+            for(String line : Files.readAllLines(INPUT)) {
+                writer.write(line.split("\\s").length);
                 writer.write("\n");
             }
         } catch (IOException exception) {
             exception.printStackTrace();
         }
     }
-    private static void findWords(String[] args,String word) {
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(PATH.toFile(),true))) {
-            for(String str : args) {
+    private static void findWords(String word) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(OUTPUT.toFile(),true))) {
+            for(String line : Files.readAllLines(INPUT)) {
                 int index = -1;
                 int count = 0;
-                while((index = word.indexOf(word,index)) >= 0) {
+                while((index = line.indexOf(word,index)) >= 0) {
                     count++;
                 }
                 writer.write(count);
